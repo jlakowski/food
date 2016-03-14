23784fd38f4a85d2ff4afb56c36b78233dd944f6
Jim Lakowski <jimlakowski@gmail.com>
2016-03-14 16:33:53 -0500

    refactored it to make it easier for a monte carlo simulation

--- optimizefood.py
+++ optimizefood.py
@@ -3,70 +3,92 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-with open('calories.csv', 'rb') as f:
-    reader = csv.reader(f)
-    caldata = list(reader)
-meatwords = ['pork', 'beef', 'sausage', 'chicken', 'clams', 'oysters', 'cod','salmon','herring', 'haddock', 'halibut', 'fish', 'crab','lobster','gelatin', 'lamb','deer', 'duck', 'tuna', 'turkey','milk', 'veal', 'trout', 'salami', 'sardines', 'flounder', 'shrimp', 'perch', 'cheese', 'scallops', 'braunschweiger']
-meatind = []
+def nutritionMax(targetvec, veganmode):
+    with open('calories.csv', 'rb') as f:
+        reader = csv.reader(f)
+        caldata = list(reader)
+    meatwords = ['pork', 'beef', 'sausage', 'chicken', 'clams', 'oysters', 'cod','salmon','herring', 'haddock', 'halibut', 'fish', 'crab','lobster','gelatin', 'lamb','deer', 'duck', 'tuna', 'turkey','milk', 'veal', 'trout', 'salami', 'sardines', 'flounder', 'shrimp', 'perch', 'cheese', 'scallops', 'braunschweiger', 'bologna']
+    meatind = []
 
-veganmode = True
-for k in range(2, len(caldata)):
-    for i in range(2,len(caldata[2])):
-        caldata[k][i] = float(caldata[k][i])
-    for word in meatwords:
-        if(word in caldata[k][0].lower()):
-            meatind.append(k)
+    #veganmode = True
+    for k in range(2, len(caldata)):
+        for i in range(2,len(caldata[2])):
+            caldata[k][i] = float(caldata[k][i])
+        for word in meatwords:
+            if(word in caldata[k][0].lower()):
+                meatind.append(k)
 
-if veganmode:
-    for k in range(len(caldata), 2, -1):
-        if k in meatind:
-            caldata.pop(k)
+    if veganmode:
+        for k in range(len(caldata), 2, -1):
+            if k in meatind:
+                caldata.pop(k)
 
-cdnp = np.asarray(caldata)
-cdnpt = np.transpose(cdnp)
+    cdnp = np.asarray(caldata)
+    cdnpt = np.transpose(cdnp)
 
-caldens = sorted(caldata, key=lambda x: x[7],reverse=True)
-protdens = sorted(caldata, key=lambda x: x[8],reverse=True)
-carbdens = sorted(caldata, key=lambda x: x[9],reverse=True)
+    #caldens = sorted(caldata, key=lambda x: x[7],reverse=True)
+    #protdens = sorted(caldata, key=lambda x: x[8],reverse=True)
+    #carbdens = sorted(caldata, key=lambda x: x[9],reverse=True)
 
-caldvals = cdnpt[7][2:].astype(float)
-protdval = cdnpt[8][2:].astype(float)
-carbdval = cdnpt[9][2:].astype(float)
+    #create histograms for these
+    caldvals = cdnpt[7][2:].astype(float)
+    protdval = cdnpt[8][2:].astype(float)
+    carbdval = cdnpt[9][2:].astype(float)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    A = np.array([caldvals, protdval, carbdval])
+    At= np.transpose(A)
 
-ax.scatter(caldvals, protdval, carbdval)
+    pdm = protdval.mean()
+    cdm = caldvals.mean()
+    cbdm = carbdval.mean()
+    #this is the targer vector, plane, for (caldensity, protdensity, carbdensity)
+    #b = np.array([cdm,pdm,cbdm])
+    b = np.asarray(targetvec)
+    LHS = np.dot(At, A) #this is singular
+    # so we're going to do this
+    #http://math.stackexchange.com/questions/381600/singular-matrix-problem
+    #https://see.stanford.edu/materials/lsoeldsee263/08-min-norm.pdf
+    # You can also use a QR
+    # xb = At (A At)^-1 b
+    MID  = np.linalg.inv(np.dot(A, At))
+    L = np.dot(At, MID)
 
-ax.set_xlabel('Calorie Density (kCal/g)')
-ax.set_ylabel('Protien Density, (g_(protien)/g')
-ax.set_zlabel('Carb Density (g_(carb)/g')
-#see if there are some clusters using a clustering algorithm
+    xb = np.dot(L,b) #this should be the weighting to get us near the plane
 
+    return [caldata, xb]
 
+def plotResults(caldata):
+    cdnp = np.asarray(caldata)
+    cdnpt = np.transpose(cdnp)
 
-A = np.array([caldvals, protdval, carbdval])
-At= np.transpose(A)
+    #caldens = sorted(caldata, key=lambda x: x[7],reverse=True)
+    #protdens = sorted(caldata, key=lambda x: x[8],reverse=True)
+    #carbdens = sorted(caldata, key=lambda x: x[9],reverse=True)
 
-pdm = protdval.mean()
-cdm = caldvals.mean()
-cbdm = carbdval.mean()
-#this is the ideal formula for (caldensity, protdensity, carbdensity)
+    #create histograms for these
+    caldvals = cdnpt[7][2:].astype(float)
+    protdval = cdnpt[8][2:].astype(float)
+    carbdval = cdnpt[9][2:].astype(float)
 
-b = np.array([cdm,0.8,cbdm])
+    caldvalshist = plt.hist(caldvals)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-LHS = np.dot(At, A) #this is singular
-# so we're going to do this
-#http://math.stackexchange.com/questions/381600/singular-matrix-problem
-MID  = np.linalg.inv(np.dot(A, At))
-L = np.dot(At, MID)
+    ax.scatter(caldvals, protdval, carbdval)
 
-xb = np.dot(L,b) #this should be the weighting
+    ax.set_xlabel('Calorie Density (kCal/g)')
+    ax.set_ylabel('Protien Density, (g_(protien)/g')
+    ax.set_zlabel('Carb Density (g_(carb)/g')
+    
+    plt.show()
 
-resmat = []
-for i in range(len(xb)):
-    resmat.append([caldata[i+2][0], xb[i]])
-rmsort = sorted(resmat, key=lambda x: x[1], reverse=True)
+def main():
+    plt.close("all")
+    caldata, xb = nutritionMax([1.0,1.0,1.0], veganmode=True)
+    resmat = []
+    for i in range(len(xb)):
+        resmat.append([caldata[i+2][0], xb[i]])
+    rmsort = sorted(resmat, key=lambda x: x[1], reverse=True)
 
-
-#plt.show()
+if __name__ == "__main__":
+    main()
